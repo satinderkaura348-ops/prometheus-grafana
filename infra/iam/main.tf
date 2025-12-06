2265da202f920938b4012d7c6f1df61f52d758c7
satinderkaura348-ops <satinderkaura348@gmail.com>
2025-12-06 23:45:43 +0000

    added infra

---
+++ infra/iam/main.tf
@@ -0,0 +1,108 @@
+resource "aws_eks_cluster" "cluster" {
+  name = "monitoring_cluster"
+
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = [
+  aws_subnet.private["monitoring-private-1"].id,
+  aws_subnet.private["monitoring-private-2"].id,
+  aws_subnet.private["monitoring-private-3"].id
+]
+
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "monitoring_cluster_iam"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+
+resource "aws_iam_role" "node" {
+    name = "monitoring_node_group"
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["sts:AssumeRole"]
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodeMinimalPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "monitoring_node_group"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = [for s in aws_subnet.private : s.id]
+
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodeMinimalPolicy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryPullOnly,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
+  ]
+
+  tags = {
+  Environment = "Dev"
+  Project     = "Prometheus-Grafana"
+}
+
+}
---
+++ infra/vpc/variables.tf
@@ -0,0 +1,37 @@
+variable "vpc_cidr" {
+    default = 10.0.0.0/16
+    description = "CIDR block for the VPC"
+}
+
+variable "vpc_name" {
+    default = "monitoring-vpc"
+    description = "Name for VPC"
+}
+
+variable "igw_name" {
+    default = "monitoring-igw"
+    description = "Name for igw"
+}
+
+variable "route_table" {
+    default = "monitoring-route-table"
+    description = "Name for route table"
+}
+
+variable "public_subnets" {
+    description = "Map of public subnet names to CIDR"
+    default = {
+        "monitoring-public-1" = "10.0.1.0/20"
+        "monitoring-public-2" = "10.0.2.0/20"
+        "monitoring-public-3" = "10.0.3.0/20"
+    }
+}
+
+variable "private_subnets" {
+    description = "Map of private subnet names to CIDR"
+    default = {
+        "monitoring-private-1" = "10.0.4.0/20"
+        "monitoring-private-2" = "10.0.5.0/20"
+        "monitoring-private-3" = "10.0.6.0/20"
+    }
+}
