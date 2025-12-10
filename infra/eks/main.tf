635ac3d6ee8c5f841327da039c9c0ccbe0e90478
satinderkaura348-ops <satinderkaura348@gmail.com>
2025-12-10 21:14:11 +0000

    updated version to 22.0.0

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -1,23 +1,27 @@
 #eks/main.tf
 
+data "aws_caller_identity" "current" {}
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "21.10.1"  
+  source  = "terraform-aws-modules/eks/aws"
+  version = "22.0.0"
+
   name    = var.cluster_name
   kubernetes_version = "1.33"
-  subnet_ids = var.subnet_ids
-  vpc_id = var.vpc_id
-  create_kms_key  = false
+  subnet_ids      = var.subnet_ids
+  vpc_id          = var.vpc_id
 
+  # Disable custom KMS key creation
+  create_kms_key = false
+
+  # THIS IS THE KEY FIX FOR v21+
+  enable_cluster_encryption_config = false   # ← disables the entire block
+  cluster_encryption_config        = []      # ← must be empty list when disabled
+
+  # Disable CloudWatch log group creation
   create_cloudwatch_log_group = false
-  
-  tags = {
-    Environment = "Dev"
-    Project     = "Prometheus-Grafana"
-}
 
-  # Node Groups
+  # Node groups
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
@@ -33,8 +37,11 @@ module "eks" {
       max_size       = 2
       desired_size   = 1
     }
+  }
 
-
+  tags = {
+    Environment = "Dev"
+    Project     = "Prometheus-Grafana"
   }
 }
 
