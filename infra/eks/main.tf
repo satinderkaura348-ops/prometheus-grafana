bb786632b8b26bf1922e9a32a99ea5052212f11d
satinderkaura348-ops <satinderkaura348@gmail.com>
2025-12-10 03:28:33 +0000

    increase instance size

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -18,14 +18,14 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
       min_size       = 1
       max_size       = 3
       desired_size   = 2
     }
     two = {
       name           = "node-group-2"
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
