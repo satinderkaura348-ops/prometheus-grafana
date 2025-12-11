2dbc79d45f77f06d0ece3c61ee765076f75a42c9
satinderkaura348-ops <satinderkaura348@gmail.com>
2025-12-11 00:58:05 +0000

    modified:   infra/eks/main.tf         new file:   on the node role         new file:   required policies attached

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -29,14 +29,14 @@ module "eks" {
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
       min_size       = 1
       max_size       = 3
       desired_size   = 2
     }
     two = {
       name           = "node-group-2"
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
